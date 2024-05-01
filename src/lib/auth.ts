ba4f8f68864018f5d39e5d14a00111e5a18a8675
fernand0cruz <cruzfernando.dev@gmail.com>
2024-05-01 14:47:00 -0300

    Fix: Update auth.ts to use correct import for next-auth/adapters

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,11 +2,12 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
 import GoogleProvider from "next-auth/providers/google";
+import { Adapter } from "next-auth/adapters";
 
 
 export const authOptions: AuthOptions = {
 
-    adapter: PrismaAdapter(prismaClient) as any,
+    adapter: PrismaAdapter(prismaClient) as Adapter,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || '',

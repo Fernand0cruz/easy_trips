f3a2926399e6098aff7fda20f87d7c274bd6a344
fernand0cruz <cruzfernando.dev@gmail.com>
2024-05-03 13:40:55 -0300

    feat: Update QuickSearch component to fetch trip categories from the database

--- src/app/components/quickSearch.tsx
+++ src/app/components/quickSearch.tsx
@@ -1,19 +1,20 @@
 import { Card } from "@/components/ui/card";
-import { quickSearch } from "@/config/constants";
 import Link from "next/link";
 import Image from "next/image";
+import { prismaClient } from "@/lib/prisma";
 
-const QuickSearch = () => {
+const QuickSearch = async () => {
+    const category = await prismaClient.tripCategory.findMany({})
     return (
-        <div className="py-5 my-5 flex flex-col gap-5">
+        <div className="mt-10 flex flex-col gap-5">
             <h1 className="uppercase font-bold text-center">Buscas Rápidas</h1>
             <div className="grid grid-cols-2 gap-3 md:grid md:grid-cols-4 md:gap-3">
                 {
-                    quickSearch.map((item, index) => (
-                        <Link key={index} href={item.title}>
+                    category.map((item, index) => (
+                        <Link key={index} href={"category/"+item.slug}>
                             <Card className="p-5 flex flex-col justify-center items-center hover:bg-accent">
-                                <Image src={item.image?? ""} alt={item.title} width={50} height={50} />
-                                <h1 className="text-center">{item.title}</h1>
+                                <Image src={item.image?? ""} alt={item.name} width={50} height={50} />
+                                <h1 className="text-center">{item.name}</h1>
                             </Card>
                         </Link>
                     ))

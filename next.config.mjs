255205fb1c91f1a9e0b0ca97eaecb7353f9c291d
fernand0cruz <cruzfernando.dev@gmail.com>
2024-05-03 13:40:15 -0300

    chore: Update next.config.mjs to include additional CDN hostnames

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'a0.muscache.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.guiademoteis.com.br'
             }
         ]
     }

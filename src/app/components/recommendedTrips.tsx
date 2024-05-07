d1b856562fdc03d811e1bc38e7222bb8d144c3fa
fernand0cruz <cruzfernando.dev@gmail.com>
2024-05-07 10:16:29 -0300

    Refactor: RecommendedTrips component and remove unnecessary code

--- src/app/components/recommendedTrips.tsx
+++ src/app/components/recommendedTrips.tsx
@@ -2,13 +2,9 @@ import { prismaClient } from "@/lib/prisma";
 import Trips from "./trips";
 
 const RecommendedTrips = async () => {
-    const trips = await prismaClient.trip.findMany({
-        include: {
-            category: true,
-        }
-    })
+    const trips = await prismaClient.trip.findMany({})
     return (
-        <div className="mt-10 flex flex-col gap-5">
+        <div className="flex flex-col gap-5">
             <h1 className="uppercase font-bold text-center">Viagens recomendadas!</h1>
             <div className="grid grid-cols-2 gap-3 md:grid md:grid-cols-4">
                 <Trips data={trips}/>

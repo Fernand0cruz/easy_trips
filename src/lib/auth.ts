7c39f40f45e3bc3fc3c92cef01bf175d695eff70
fernand0cruz <cruzfernando.dev@gmail.com>
2024-05-17 15:47:37 -0300

    Refactor: Update auth.ts to remove unused code and improve session handling

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,14 +13,4 @@ export const authOptions: AuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
-    callbacks: {
-        async session({ session, token, user}) {
-            session.user = {...session.user, id: user.id} as {
-                id: string;
-                name: string;
-                email: string;
-            }
-            return session
-        }
-    },
 }

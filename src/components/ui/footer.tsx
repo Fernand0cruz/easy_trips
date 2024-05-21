2810b0bee7c4308e300c05e58de46114988d0091
fernand0cruz <cruzfernando.dev@gmail.com>
2024-05-21 20:25:30 -0300

    Refactor:  Update footer component to improve layout and add max width container

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <Card className="mt-10 p-5">
-            <h1>Copyright (C) 2023-2024 <Link href={"https://github.com/Fernand0cruz"} className="font-bold">Fernand0W</Link></h1>
+        <Card className="mt-5 p-5">
+            <div className="max-w-7xl m-auto">
+                <h1>Copyright (C) 2024 <Link href={"https://github.com/Fernand0cruz"} className="font-bold">Fernand0W</Link></h1>
+            </div>
         </Card>
     );
 }

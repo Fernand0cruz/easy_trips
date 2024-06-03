9a501549498b61bc3d54c0d85ffdcab2435c3927
fernand0cruz <cruzfernando.dev@gmail.com>
2024-06-03 14:24:54 -0300

    feat: Refactor GET route for user trip reservations

--- src/app/api/trips/user/[userId]/reservations/route.ts
+++ src/app/api/trips/user/[userId]/reservations/route.ts
@@ -1,26 +1,23 @@
 import { prismaClient } from "@/lib/prisma"
 import { NextResponse } from "next/server"
 
-export async function GET(request: Request, { params: { userId } }: { params: { userId: string } }){
-    if(!userId){
-        return {
-            status: 400,
-            body: {
-                message: "Missing userId"
-            }
-        }
+export async function GET(request: Request, { params: { userId } }: { params: { userId: string } }) {
+    if (!userId) {
+        return new NextResponse(JSON.stringify({ message: "Missing userId" }), { status: 400 });
     }
 
-    const reservation = await prismaClient.reservations.findMany({
-        where: {
-            userId: userId
-        },
-        include: {
-            trip: true
-        }
-    })
+    try {
+        const reservations = await prismaClient.reservations.findMany({
+            where: {
+                userId: userId
+            },
+            include: {
+                trip: true
+            }
+        });
 
-    return new NextResponse(JSON.stringify(reservation), {
-        status: 200,
-    })
+        return new NextResponse(JSON.stringify(reservations), { status: 200 });
+    } catch (error) {
+        return new NextResponse(JSON.stringify({ message: "Internal Server Error" }), { status: 500 });
+    }
 }
